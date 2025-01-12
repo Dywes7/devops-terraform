4312af4aeb6826cb7bc3e1f93198590a0e604f59
Dywes7 <dywes800@gmail.com>
2025-01-11 21:19:30 -0300

    add .tf loadbalancer haproxy

---
+++ terraform_openstack/haproxy.tf
@@ -0,0 +1,39 @@
+resource "openstack_compute_instance_v2" "haproxy" {
+  name            = "haproxy-lb"
+  image_id        = "dcf49c5a-0fc1-40a9-b26e-fd42c24516d6"  # Altere para a imagem desejada
+  flavor_id       = "cfb3f68f-ea52-4ed5-a40f-8b82befccc3f"   # Altere para o tipo de máquina desejado
+  key_pair        = "windows-home"
+  security_groups = ["default"]
+  network {
+    name = "wan"
+    fixed_ip_v4 = "200.19.179.209"
+  }
+  user_data = <<-EOT
+    #!/bin/bash
+    apt-get update -y
+    apt-get install -y haproxy
+
+    # Adiciona a configuração do HAProxy
+    echo "
+    frontend http_front
+        bind *:80
+        default_backend http_back
+
+    backend http_back
+        balance roundrobin
+        server inst1 192.168.159.100:80 check
+        server inst2 192.168.159.101:80 check
+
+    # Painel de monitoramento
+    frontend stats
+        bind *:9000
+        mode http
+        log global
+        stats enable
+        stats uri /stats
+        stats auth Admin:senha
+    " >> /etc/haproxy/haproxy.cfg
+
+    systemctl restart haproxy
+  EOT
+}
